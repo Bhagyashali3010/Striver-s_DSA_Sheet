85600b8f5fc7ff6742370815b3aac463202f1c10
Bhagyashali Shinde <shindebhagyashali3010@gmail.com>
2026-04-23 18:30:45 +0530

    Solved some more basic problems on recurssion

--- recursion.py
+++ recursion.py
@@ -51,9 +51,46 @@
 
 # print(sumOfNum(10))
 
-def factorial(n):
-    if n==0 or n==1:
-        return 1
-    return n*factorial(n-1)
+# def factorial(n):
+#     if n==0 or n==1:
+#         return 1
+#     return n*factorial(n-1)
 
-print(factorial(4))
+# print(factorial(4))
+
+#23rd April
+#Reverse Arr
+
+# def reverseArr(arr, i, n):
+#       if i>=n//2:
+#          return
+#       arr[i], arr[n-i-1]=arr[n-i-1], arr[i]
+#       reverseArr(arr, i+1, n)
+    
+# arr=[1,2,3,4,5]
+# print(reverseArr(arr, 0, len(arr)))
+# print(arr)
+
+
+#palindrom String
+
+# s="haanaah"
+# def palindrom(s, i):
+#     if i>=len(s)//2:
+#         return True
+#     if s[i]!=s[len(s)-1-i]:
+#         return False
+#     return palindrom(s, i+1)
+# print(palindrom(s,0))
+    
+
+
+#Fibonacci
+def fibonacci(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    return fibonacci(n-1)+ fibonacci(n-2)
+
+print(fibonacci(8))
