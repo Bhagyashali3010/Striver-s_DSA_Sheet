9a2962d7b177f6d7492ce70cc7c3459333fa4787
Bhagyashali Shinde <shindebhagyashali3010@gmail.com>
2026-04-24 23:21:43 +0530

    Solved 2 more basic hashing problem using dictionary

--- hashing.py
+++ hashing.py
@@ -9,11 +9,36 @@
 
 #charecter hashing
 
-arr=['a','b','a','c','b']
-hash=[0]*26
-for i in range (len(arr)):
-    hash[ord(arr[i])-ord('a')]+=1
+# arr=['a','b','a','c','b']
+# hash=[0]*26
+# for i in range (len(arr)):
+#     hash[ord(arr[i])-ord('a')]+=1
 
-print("n",'->',hash[ord('n')-ord('a')])
-print("b",'->',hash[ord('b')-ord('a')])
-print("c",'->',hash[ord('c')-ord('a')])
+# print("n",'->',hash[ord('n')-ord('a')])
+# print("b",'->',hash[ord('b')-ord('a')])
+# print("c",'->',hash[ord('c')-ord('a')])
+
+
+#hashing using unordered map(dictionary)
+# from collections import defaultdict
+
+# arr=[2,4,5,6,9,2,9]
+# dicti=defaultdict(int)
+
+# for i in range(len(arr)):
+#     dicti[arr[i]]+=1
+
+# for i, j in dicti.items():
+#     print(i, j)
+
+
+#Maximum occurance of ele in array
+from collections import defaultdict
+
+arr=[2,4,5,6,9,2,9,9]
+dicti=defaultdict(int)
+
+for i in range(len(arr)):
+    dicti[arr[i]]+=1
+
+print(max(dicti.values()))
